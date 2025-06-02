01142729d84fae05c0be77dddaf845325bff25e4
LinhDoHoangDSV <linhdh@dgroup.co>
2025-06-03 02:17:19 +0700

    test deploy

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,8 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // ✅ Middleware
   app.use(cookieParser());
 
-  // ✅ CORS cấu hình tập trung
   const allowedOrigins = [
     'http://localhost:3000',
     'http://localhost:3001',
@@ -25,16 +23,13 @@ async function bootstrap() {
     allowedHeaders: ['Content-Type', 'Accept', 'Authorization', 'Refresh'],
   });
 
-  // ✅ Global API prefix
   app.setGlobalPrefix('/api/v1');
 
-  // ✅ Swagger
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('docs', app, document, {
     useGlobalPrefix: true,
   });
 
-  // ✅ Start server
   await app.listen(3010);
 }
 bootstrap();
